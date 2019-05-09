74ae8c1ba680523e43042e0251157b72a8e70cd0
Eric Garside <garside@gmail.com>
2019-05-09 10:32:19 -0400

    Callbacks

--- lib/batch_processor/batch/callbacks.rb
+++ lib/batch_processor/batch/callbacks.rb
@@ -9,6 +9,22 @@ module BatchProcessor
       included do
         include ActiveSupport::Callbacks
         define_callbacks :initialize
+        define_callbacks_with_handler :job_started, :job_retrying, :job_performed, :job_canceled,
+                                      :batch_aborted, :batch_finished, :batch_success, :batch_errors, :batch_completed
+      end
+
+      class_methods do
+        private
+
+        def define_callbacks_with_handler(*events, handler: :on, filter: :after)
+          define_callbacks(*events)
+
+          events.each do |event|
+            define_singleton_method("#{handler}_#{event}".to_sym) do |*filters, &block|
+              set_callback(event, filter, *filters, &block)
+            end
+          end
+        end
       end
     end
   end
