fab25c8c475244d76ff234398665b496bb827838
Eric Garside <garside@gmail.com>
2019-06-03 09:09:19 -0400

    Job Results (Success & Failure)

--- lib/batch_processor/batch/job_controller.rb
+++ lib/batch_processor/batch/job_controller.rb
@@ -6,8 +6,16 @@ module BatchProcessor
     module JobController
       extend ActiveSupport::Concern
 
+      class_methods do
+        private
+
+        def job_callbacks_with_handler(*events)
+          define_callbacks_with_handler(*events.map { |event| "job_#{event}".to_sym })
+        end
+      end
+
       included do
-        define_callbacks_with_handler :job_enqueued, :job_running, :job_retried, :job_canceled
+        job_callbacks_with_handler :enqueued, :running, :retried, :canceled, :success, :failure
       end
 
       def job_enqueued
@@ -35,6 +43,28 @@ module BatchProcessor
           details.pipelined do
             details.increment(:total_retries_count)
             details.increment(:pending_jobs_count)
+            details.decrement(:running_jobs_count)
+          end
+        end
+      end
+
+      def job_success
+        raise BatchProcessor::BatchNotProcessingError unless processing?
+
+        run_callbacks(__method__) do
+          details.pipelined do
+            details.increment(:successful_jobs_count)
+            details.decrement(:running_jobs_count)
+          end
+        end
+      end
+
+      def job_failure
+        raise BatchProcessor::BatchNotProcessingError unless processing?
+
+        run_callbacks(__method__) do
+          details.pipelined do
+            details.increment(:failed_jobs_count)
             details.decrement(:running_jobs_count)
           end
         end
